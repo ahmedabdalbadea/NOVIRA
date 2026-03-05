32d7c95cd81eb64e4618175b035c49fccb0235ee
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-03-05 15:06:42 +0200

    create onboarding_card_action section

---
+++ lib/features/splash/views/widgets/onboarding_card_actions.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:novira_app/constants.dart';
+import 'package:novira_app/core/utils/styles.dart';
+import 'package:novira_app/core/widgets/custom_elevated_button.dart';
+
+class OnboardingCardActions extends StatelessWidget {
+  const OnboardingCardActions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.sizeOf(context).width * 0.66,
+          child: Text(
+            "Let's begin your journey toward better\n well-being. We'll personalize your\n experience.",
+            style: Styles.textStyle14.copyWith(color: kDesTextColor),
+            maxLines: 3,
+            textAlign: TextAlign.center,
+          ),
+        ),
+
+        const SizedBox(height: 28),
+
+        CustomElevatedButton(
+          title: "Start Your Journey",
+          gradient: kButtonGradient,
+          onPressed: () {},
+        ),
+
+        const SizedBox(height: 16),
+        GestureDetector(
+          onTap: () {},
+          child: Text(
+            "I already have an account",
+            style: Styles.textStyle14.copyWith(color: kDesTextColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
