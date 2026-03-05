f9626e99d41ccca050cbf301bf4461ae144c525e
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-03-05 15:06:20 +0200

    add onboarding card mood check in section

---
+++ lib/features/splash/views/widgets/onboarding_card_mood_check_in.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:novira_app/core/utils/styles.dart';
+import 'package:novira_app/features/splash/views/widgets/mood_selector_grid_view.dart';
+
+class OnboardingCardMoodCheckIn extends StatelessWidget {
+  const OnboardingCardMoodCheckIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.sizeOf(context).width * 0.6,
+          child: const Text(
+            "How are you feeling today?",
+            style: Styles.textStyle24,
+            textAlign: TextAlign.center,
+            maxLines: 2,
+          ),
+        ),
+
+        const SizedBox(height: 28),
+        MoodSelectorGridView(),
+      ],
+    );
+  }
+}
