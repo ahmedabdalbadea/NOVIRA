7371ad5cbcd6cbfb7f16ee01edd8bdf13891e72c
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-03-10 17:36:15 +0200

    add custom logo and create your safe space to signup body

--- lib/features/auth/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/views/widgets/sign_up_view_body.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:novira_app/constants.dart';
+import 'package:novira_app/core/utils/styles.dart';
+import 'package:novira_app/core/widgets/custom_logo.dart';
 import 'package:novira_app/features/splash/views/widgets/onboarding_card_body.dart';
+import 'package:novira_app/generated/l10n.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key, required this.mood});
   final String mood;
   @override
   Widget build(BuildContext context) {
-    return OnboardingCardBody(body: Text(mood));
+    return OnboardingCardBody(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CustomLogo(),
+          const SizedBox(height: 8),
+          Align(
+            alignment: AlignmentGeometry.center,
+            child: Text(
+              S.of(context).createYourSafeSpace,
+              style: Styles.textStyle24,
+            ),
+          ),
+          const SizedBox(height: 16),
+
+        ],
+      ),
+    );
   }
+
+
 }
