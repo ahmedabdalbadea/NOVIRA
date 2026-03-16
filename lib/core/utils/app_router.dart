9708e69d853806ea4858574ff9efd053c63e5bf7
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-03-16 19:20:10 +0200

    add sliding animation with navigation

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:novira_app/features/auth/views/log_in_view.dart';
 import 'package:novira_app/features/auth/views/sign_up_view.dart';
@@ -17,19 +18,59 @@ abstract class AppRouter {
 
       GoRoute(
         path: kSignUpView,
-
-        builder: (context, state) {
+        pageBuilder: (context, state) {
           final String mood = state.extra as String;
-          return SignUpView(mood: mood);
+
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: SignUpView(mood: mood),
+            transitionDuration: const Duration(milliseconds: 600),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  const begin = Offset(1.0, 0.0);
+                  const end = Offset.zero;
+                  const curve = Curves.easeInOutCubic;
+                  var tween = Tween(
+                    begin: begin,
+                    end: end,
+                  ).chain(CurveTween(curve: curve));
+                  var offsetAnimation = animation.drive(tween);
+
+                  return SlideTransition(
+                    position: offsetAnimation,
+                    child: child,
+                  );
+                },
+          );
         },
       ),
 
       GoRoute(
         path: kLogInView,
-
-        builder: (context, state) {
+        pageBuilder: (context, state) {
           final String mood = state.extra as String;
-          return LogInView(mood: mood);
+
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: LogInView(mood: mood),
+            transitionDuration: const Duration(milliseconds: 600),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+                  const begin = Offset(1.0, 0.0);
+                  const end = Offset.zero;
+                  const curve = Curves.easeInOutCubic;
+                  var tween = Tween(
+                    begin: begin,
+                    end: end,
+                  ).chain(CurveTween(curve: curve));
+                  var offsetAnimation = animation.drive(tween);
+
+                  return SlideTransition(
+                    position: offsetAnimation,
+                    child: child,
+                  );
+                },
+          );
         },
       ),
     ],
