d7ac1758986b21e4d7fc2597f318977ac82e8f32
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-03-16 17:46:22 +0200

    add staggered animation to splash view body

--- lib/features/splash/views/widgets/splash_view_body.dart
+++ lib/features/splash/views/widgets/splash_view_body.dart
@@ -2,16 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:novira_app/features/splash/views/widgets/icon_language_button.dart';
 import 'package:novira_app/features/splash/views/widgets/onboarding_card.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  late Animation<double> languageIconAnimation;
+  late Animation<double> cardAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1400),
+    );
+
+    languageIconAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.7, 0.9, curve: Curves.easeOut),
+      ),
+    );
+
+    cardAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.7, curve: Curves.easeOut),
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        IconLanguageButton(),
+        FadeTransition(
+          opacity: languageIconAnimation,
+          child: const IconLanguageButton(),
+        ),
         const SizedBox(height: 6),
-        OnboardingCard(),
+        FadeTransition(
+          opacity: cardAnimation,
+          child: SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(0, 0.2),
+              end: Offset.zero,
+            ).animate(cardAnimation),
+            child: const OnboardingCard(),
+          ),
+        ),
       ],
     );
   }
