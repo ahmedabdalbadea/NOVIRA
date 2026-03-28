f44916b2c4370de434643773991daa834c09b85e
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-03-28 13:26:09 +0200

    ceate auth cubit and determine state of sign up

---
+++ lib/features/auth/manager/auth_cubit/auth_state.dart
@@ -0,0 +1,16 @@
+part of 'auth_cubit.dart';
+
+@immutable
+sealed class AuthState {}
+
+final class AuthInitial extends AuthState {}
+
+final class SignUpLoading extends AuthState {}
+
+final class SignUpSuccess extends AuthState {}
+
+final class SignUpFailure extends AuthState {
+  final String errMsg;
+
+  SignUpFailure(this.errMsg);
+}
