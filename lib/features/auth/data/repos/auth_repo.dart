0887841205dee7b29f02c86e99d5535bdd732d28
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-12 21:27:07 +0200

    ignore auth reop

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -1,8 +1,8 @@
-import 'package:dartz/dartz.dart';
-import 'package:novira_app/core/errors/failure.dart';
-import 'package:novira_app/core/models/user_model.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:novira_app/core/errors/failure.dart';
+// import 'package:novira_app/core/models/user_model.dart';
 
-abstract class AuthRepo {
-  Either<Failure, UserModel> signUp({required UserModel user});
-  Either<Failure, UserModel> logIn({required UserModel user});
-}
+// abstract class AuthRepo {
+//   Either<Failure, UserModel> signUp({required UserModel user});
+//   Either<Failure, UserModel> logIn({required UserModel user});
+// }
