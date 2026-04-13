fa67cc706cebf1db878bc0f68de8807627fdbafd
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-13 21:37:39 +0200

    create question model

---
+++ lib/features/assessment/data/models/question_model.dart
@@ -0,0 +1,11 @@
+class QuestionModel {
+  final String description;
+  final String question;
+  final List<String> answers;
+
+  const QuestionModel({
+    required this.description,
+    required this.question,
+    required this.answers,
+  });
+}
