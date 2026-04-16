8ce99b90d753aeae49b29db6ece5a394f27f7492
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-04-17 00:19:17 +0200

    add fetch questions in question cubit

--- lib/features/assessment/manager/question_cubit/question_cubit.dart
+++ lib/features/assessment/manager/question_cubit/question_cubit.dart
@@ -1,8 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:novira_app/features/assessment/data/models/question_model/question_model.dart';
+
+import 'package:novira_app/features/assessment/data/repos/assessement_repo_impl.dart';
 
 part 'question_state.dart';
 
 class QuestionCubit extends Cubit<QuestionState> {
-  QuestionCubit() : super(QuestionInitial());
+  QuestionCubit(this._assessementRepoImpl) : super(QuestionInitial());
+  final AssessementRepoImpl _assessementRepoImpl;
+  List<QuestionModel> questionsList = [];
+  Future<void> fetchQuestions({String lang = "ar"}) async {
+    emit(QuestionLoading());
+    var data = await _assessementRepoImpl.fetchQuestion(lang);
+    data.fold(
+      (failure) {
+        emit(QuestionFailure(failure.errMsg));
+      },
+      (questions) {
+        questionsList = questions;
+        emit(QuestionSuccess());
+      },
+    );
+  }
 }
