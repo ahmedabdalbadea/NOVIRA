e34c7ead25007216e29625a0c43c3a87aef846ba
Ahmed Abd Albadea <ahmedmohmaedabdalbadea@gmail.com>
2026-05-18 17:52:48 +0300

    create ai chat view

---
+++ lib/features/home/view/ai_chat_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:novira_app/features/home/view/widgets/ai_chat_view_body.dart';
+
+class AiChatView extends StatelessWidget {
+  const AiChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: AiChatViewBody());
+  }
+}
